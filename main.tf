6d8797832750e0dc717c85f52a6631ea67ac7b8b
TakuoIshida <archersparadox.100@gmail.com>
2021-07-23 17:34:55 +0900

    create vpc,rt,subnet OK

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 locals {
   default_ami  = "ami-0c3fd0f5d33134a76"
   project_name = "hoge"
+  az_a         = "ap-northeast-1a"
+  az_c         = "ap-northeast-1c"
 }
 
 # module "web_server" {
@@ -80,3 +82,58 @@ locals {
 #     }
 #   }
 # }
+
+#7 章 ネットワーク
+
+resource "aws_vpc" "example" {
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    "Name" = "hogehoge_tag" //nameはリソース名にも表示されるのであると見やすい
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_a
+  tags = {
+    "Name" = join("-", [local.az_a, "public_1a"])
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_c
+  tags = {
+    "Name" = join("-", [local.az_c, "public_1c"])
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public_rt.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.public_rt.id
+}
