7b5f937156dd5d28d067b81742c39b7d2874bb82
TakuoIshida <archersparadox.100@gmail.com>
2021-07-23 17:50:44 +0900

    create private-subnet OK

--- main.tf
+++ main.tf
@@ -128,6 +128,15 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_route_table_association" "public_1a" {
   subnet_id      = aws_subnet.public_1a.id
   route_table_id = aws_route_table.public_rt.id
@@ -137,3 +146,14 @@ resource "aws_route_table_association" "public_1c" {
   subnet_id      = aws_subnet.public_1c.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_subnet" "private_1a" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "192.168.3.0/24"
+  availability_zone       = local.az_a
+  map_public_ip_on_launch = false
+  tags = {
+    "Name" = join("-", [local.az_a, "private_1a"])
+  }
+}
+
